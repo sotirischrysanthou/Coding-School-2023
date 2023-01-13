f3aa888627405037e4a634aa6bfd0f518ccc6ddb
Sotiris Chrysanthou <sotririsx10@gmail.com>
2023-01-13 18:51:04 +0200

    Progrm #2 finished

--- Session-05/Session-05/MainProgram.cs
+++ Session-05/Session-05/MainProgram.cs
@@ -6,13 +6,38 @@ internal class MainProgram
 {
     private static void Main(string[] args)
     {
+        string input;
+
         /* PROGRAM #1 
-         * Write a C# program to print Hello and your name in the same line.*/
+         * Write a C# program that reverses a given string (your name)*/
         ProgramOne progOne = new ProgramOne();
         Console.WriteLine("PROGRAM #1\n----------");
-        Console.WriteLine(progOne.ReverseStringBulder());
+        Console.WriteLine("Write your name:");
+        input = Console.ReadLine();
+        Console.WriteLine(progOne.ReverseStringBulder(input));
         Console.WriteLine("\n");
 
+        /* PROGRAM #2 
+         * Write a C# program that asks the user for an integer (n) and gives them 
+         * the possibility to choose between computing the sum and computing 
+         * the product of 1,...,n*/
+        ProgramTwo progTwo = new ProgramTwo();
+        Console.WriteLine("PROGRAM #2\n----------");
+        int n;
+        Console.WriteLine("Give a Number:");
+        input = Console.ReadLine();
+        while (! input.All(char.IsDigit))
+        {
+            Console.WriteLine("You gave wrong Input");
+            Console.WriteLine("Give a Number:");
+            input = Console.ReadLine();
+        } 
+        n = Convert.ToInt32(input);
+        Console.WriteLine("Sum from 1 to n is : " + progTwo.SumFromOneToN(n));
+        Console.WriteLine("Product from 1 to n is : " + progTwo.SumFromOneToN(n));
+        
+        Console.WriteLine("\n");
 
+        Console.ReadLine();
     }
 }
