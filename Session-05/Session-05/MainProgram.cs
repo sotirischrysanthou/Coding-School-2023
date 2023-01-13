59e28ab72bdd71918e786c8ceabfee3202b2ad47
Sotiris Chrysanthou <sotririsx10@gmail.com>
2023-01-13 17:41:02 +0200

    Program #1 finished

---
+++ Session-05/Session-05/MainProgram.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Session_05;
+
+internal class MainProgram
+{
+    private static void Main(string[] args)
+    {
+        /* PROGRAM #1 
+         * Write a C# program to print Hello and your name in the same line.*/
+        ProgramOne progOne = new ProgramOne();
+        Console.WriteLine("PROGRAM #1\n----------");
+        Console.WriteLine(progOne.ReverseStringBulder());
+        Console.WriteLine("\n");
+
+
+    }
+}
---
+++ Session-05/Session-05/ProgramOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    class ProgramOne
+    {
+        //reverses string ""Sotiris Chrysanthou""
+        public StringBuilder ReverseStringBulder()
+        {
+            StringBuilder sb = new StringBuilder("Sotiris Chrysanthou");
+            char c;
+            int end = sb.Length - 1;
+            int start = 0;
+
+            while (end - start > 0)
+            {
+                c = sb[end];
+                sb[end] = sb[start];
+                sb[start] = c;
+                start++;
+                end--;
+            }
+            return sb;
+        }
+    }
+}
