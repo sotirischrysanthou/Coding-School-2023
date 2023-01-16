b33ac24c02cf57e52da7a23131c935f7327194a5
Sotiris Chrysanthou <sdi1700224@di.uoa.gr>
2023-01-16 14:46:48 +0200

    class Person implemented and tested. I added prints in each constructor. Prints are only for testing and for easier verification from you.

---
+++ Session-06/Session-06/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06 {
+    internal class Person {
+        // Properties
+        public Guid ID { get; set; }
+        public String? Name { get; set; }
+        public int Age { get; set; }
+
+        // Constractors
+        public Person() {
+            Console.WriteLine("Person Constractor called without parameters");
+        }
+
+        public Person(Guid id) : this() {
+            ID = id;
+            Console.WriteLine("Person Constractor called with parameter  (Guid id = {0})", id);
+        }
+
+        public Person(Guid id, String name) : this(id) {
+            Name = name;
+            Console.WriteLine("Person Constractor called with parameters (Guid id = {0}, String name = {1})", id, name);
+        }
+
+        public Person(Guid id, String name, int age) : this(id, name) {
+            Age = age;
+            Console.WriteLine("Person Constractor called with parameters (Guid id = {0}, String name = {1}, int age = {2})", id, name, age);
+        }
+
+        public String GetName() {
+            return Name;
+        }
+
+        public void SetName(String name) {
+            Name = name;
+        }
+    }
+}
---
+++ Session-06/Session-06/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Session_06;
+
+
+
+internal class Program {
+    private static void Main(string[] args) {
+        Console.WriteLine("Person Class test\n------------");
+        Person person = new Person(Guid.NewGuid(), "Sotiris Chrysanthou", 25);
+        Console.WriteLine(person.GetName());
+    }
+}
