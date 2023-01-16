29c8b9e147477d6a0e60f84ef223243109fc55ce
Sotiris Chrysanthou <sdi1700224@di.uoa.gr>
2023-01-16 17:08:32 +0200

    class Schedule fully implemented

---
+++ Session-06/Session-06/Schedule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06 {
+    internal class Schedule {
+        // Properties
+        public Guid ID { get; set; }
+        public Guid CourseID { get; set; }
+        public Guid ProfessorID { get; set; }
+        public DateTime Calendar { get; set; }
+
+        public Schedule() {
+
+        }
+
+        public Schedule(Guid id) : this() {
+            ID = id;
+        }
+
+        public Schedule(Guid id, Guid courseID) : this(id) {
+            CourseID = courseID;
+        }
+
+        public Schedule(Guid id, Guid courseID, Guid professorID) : this(id, courseID) {
+            ProfessorID = professorID;
+        }
+    }
+
+    public Schedule(Guid id, Guid courseID, Guid professorID, DateTime calendar) : this(id, courseID, professorID) {
+            
+    }
+}
