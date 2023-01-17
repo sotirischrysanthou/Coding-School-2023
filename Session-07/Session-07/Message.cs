6a2f691b5f8baa8244e4ec142a571cb22d80a611
Sotiris Chrysanthou <sdi1700224@di.uoa.gr>
2023-01-17 14:30:43 +0200

    Message.cs implemented

---
+++ Session-07/Session-07/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07 {
+    internal class Message {
+        // Properties
+        public Guid ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public String Content { get; set; }
+
+        // Constractors
+        public Message(Guid id, DateTime timeStamp, String content) { 
+            ID = id;
+            TimeStamp = timeStamp;
+            Content = content;
+        }
+
+    }
+}
