804b917befe58408a7a532bc0a58407f9821a64c
Sotiris Chrysanthou <sotririsx10@gmail.com>
2023-01-22 20:50:06 +0200

    Calculator update : All arrays replaced by lists. class Parser implemented.

---
+++ Session-09/CalculatorLib/CalcOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib {
+    internal class CalcOperations {
+        public enum CalcOperation {
+            Equal = 0,
+            Add = 1,
+            Subtract = 2,
+            Multiply = 3,
+            Divide = 4,
+            Exp = 5,
+            Root = 6
+        };
+    }
+}
--- Session-09/CalculatorLib/Resolver.cs
+++ Session-09/CalculatorLib/Resolver.cs
@@ -5,118 +5,36 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using calOps = CalculatorLib.CalcOperations.CalcOperation;
 
 namespace CalculatorLib {
     public class Resolver {
         // Properties
-        private String? _expresion;
-        double[] _numbers;
-        private _CalcOperation[] _operators;
-        private int _numOfNums;
-        private int _numOfOps;
-
-        private enum _CalcOperation {
-            Equal = 0,
-            Add = 1,
-            Subtract = 2,
-            Multiply = 3,
-            Divide = 4,
-            Exp = 5,
-            Root = 6
-        };
+        private Parser _parser;
+        private List<double> _numbers;
+        private List<calOps> _operators;
 
         // Constructors
         public Resolver() {
-            Init();
+            _parser = new Parser();
+            _numbers = new List<double>();
+            _operators = new List<calOps>();
         }
 
         // Methods
-
-        public void Init() {
-            _numbers = new double[100];
-            _numOfNums = 0;
-            _operators = new _CalcOperation[100];
-            _numOfOps = 0;
-        }
         public string Resolve(String expresion) {
-            Init();
-            _expresion = expresion;
-
-            // writen like this for future Update. For more complex equations
-            // with more than one operator and more than two numbers
-            //--------------------------------------------------------------------
-
-            String ops = "+-x/^=";
-            int start = 0;
-            for (int i = 0; i < _expresion.Length; i++) {
-                if (ops.Contains(_expresion[i])) {
-                    _numbers[_numOfNums] = DoubleIsolation(start, i - start);
-                    _numOfNums++;
-                    start = i + 1;
-                    RecognizeAndAddOperator(_expresion[i]);
-                } else if (_expresion[i] == '√') {
-                    start = i + 1;
-                    RecognizeAndAddOperator(_expresion[i]);
-                }
-            }
-            //----------------------------------------------------------------------
+            _parser.Parse(expresion, _numbers, _operators);
             double res = 0;
-            while (_operators[0] != _CalcOperation.Equal) {
+            while (_operators[0] != calOps.Equal) {
                 res = SimpleEquation();
             }
             return res.ToString();
         }
 
-        /* Isolate int from _expresion*/
-        private int DoubleIsolation(int start, int length) {
-            int i;
-            try {
-                String isolatedString = _expresion.Substring(start, length);
-                i = Int32.Parse(isolatedString);
-            } catch (Exception ex) {
-                Debug.WriteLine(ex.Message);
-                throw;
-            }
-            return i;
-        }
-
-        private void RecognizeAndAddOperator(char op) {
-            _CalcOperation res = _CalcOperation.Equal;
-            switch (op) {
-                case '+':
-                    res = _CalcOperation.Add;
-                    break;
-                case '-':
-                    res = _CalcOperation.Subtract;
-                    break;
-                case '/':
-                    res = _CalcOperation.Divide;
-                    break;
-                case 'x':
-                    res = _CalcOperation.Multiply;
-                    break;
-                case '^':
-                    res = _CalcOperation.Exp;
-                    break;
-                case '√':
-                    res = _CalcOperation.Root;
-                    break;
-                default:
-                    // TODO ERROR
-                    break;
-            }
-            int i = 0;
-            while (_operators[i] != _CalcOperation.Equal) { i++; }
-            _operators[i] = res;
-            if (_operators[i] != _CalcOperation.Equal) {
-                _numOfOps++;
-            }
-        }
-
         private int FindOperatorWithHighestPiority() {
-            _CalcOperation op = _operators[0];
+            calOps op = _operators[0];
             int highestPriority = 0;
-            for (int i = 0; i < _numOfOps; i++) {
+            for (int i = 0; i < _operators.Count; i++) {
                 if (_operators[i] > op) {
                     highestPriority = i;
                     op = _operators[i];
@@ -130,22 +48,22 @@ namespace CalculatorLib {
             double res = _numbers[pos];
 
             switch (_operators[pos]) {
-                case _CalcOperation.Add:
+                case calOps.Add:
                     res = _numbers[pos] + _numbers[pos + 1];
                     break;
-                case _CalcOperation.Subtract:
+                case calOps.Subtract:
                     res = _numbers[pos] - _numbers[pos + 1];
                     break;
-                case _CalcOperation.Multiply:
+                case calOps.Multiply:
                     res = _numbers[pos] * _numbers[pos + 1];
                     break;
-                case _CalcOperation.Divide:
+                case calOps.Divide:
                     res = _numbers[pos] / _numbers[pos + 1];
                     break;
-                case _CalcOperation.Exp:
+                case calOps.Exp:
                     res = Math.Pow(_numbers[pos], _numbers[pos + 1]);
                     break;
-                case _CalcOperation.Root:
+                case calOps.Root:
                     res = Math.Sqrt(_numbers[pos]);
                     break;
                 default:
@@ -157,39 +75,11 @@ namespace CalculatorLib {
         }
 
         private void ReplaceNumbersAndOparators(double res, int pos) {
-            double[] newNumbers = new double[_numbers.Length];
-            _CalcOperation[] newOperators = new _CalcOperation[_operators.Length];
-            int oldOparatorsPos = 0;
-            int newOperatorsPos = 0;
-            int oldNumbersPos = 0;
-            int newNumbersPos = 0;
-            for (oldNumbersPos = 0; oldNumbersPos < _numOfNums; oldNumbersPos++) {
-                if (oldNumbersPos == pos) {
-                    newNumbers[newNumbersPos] = res;
-                    if (_operators[pos] != _CalcOperation.Root) {
-                        oldNumbersPos++;
-                    }
-                } else {
-                    newNumbers[newNumbersPos] = _numbers[oldNumbersPos];
-                }
-                newNumbersPos++;
+            _numbers[pos] = res;
+            if (_operators[pos] != calOps.Root) {
+                _numbers.RemoveAt(pos+1);
             }
-            _numbers = newNumbers;
-            if (_operators[pos] != _CalcOperation.Root) {
-                _numOfNums--;
-            }
-
-            if (_numOfOps > 0) {
-                for (oldOparatorsPos = 0; oldOparatorsPos < _numOfOps; oldOparatorsPos++) {
-                    if (oldOparatorsPos == pos) {
-                        oldOparatorsPos++;
-                    }
-                    newOperators[newOperatorsPos] = _operators[oldOparatorsPos];
-                    newOperatorsPos++;
-                }
-                _operators = newOperators;
-                _numOfOps--;
-            }
+            _operators.RemoveAt(pos);
         }
     }
 }
