7c995581a8f508ce77b997c803c873f190f29826
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-23 18:02:42 +0200

    Transaction Added

---
+++ Session-11-Car-Service-Center/CarServiceCenterLib/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenterLib
+{
+    public class Transaction
+    {
+        //Properties 
+        public Guid ID { get; set; }  
+        public DateTime DateTime { get; set; }
+        public Guid CustomerID { get; set; }
+        public Guid CarID { get; set; }
+        public Guid ManagerID { get; set; }
+        public double TotalPrice { get; set; }
+
+        List<TransactionLines> transactionLines = new List<TransactionLine>();
+
+
+    }
+}
