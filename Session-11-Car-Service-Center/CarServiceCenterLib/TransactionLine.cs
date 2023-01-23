bf2f426cf7ae92c79346f5b8725e0e9f1d184074
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-23 18:50:25 +0200

    class Transactionline correct properties and constractor

--- Session-11-Car-Service-Center/CarServiceCenterLib/TransactionLine.cs
+++ Session-11-Car-Service-Center/CarServiceCenterLib/TransactionLine.cs
@@ -6,21 +6,24 @@ using System.Threading.Tasks;
 
 namespace CarServiceCenterLib {
     public class TransactionLine {
+        // Properties
         public Guid ID { get; set; }
         public Guid TransactionID { get; set; }
         public Guid ServiceTaskID { get; set; }
         public Guid EnginnerID { get; set; }
+        public double Hours { get; set; }
         public double PricePerHour { get; set; } //constant maybe
-
         public double Price { get; set; }
 
-        public TransactionLine(Guid transactionID, Guid serviceTaskID, Guid enginnerID, double pricePerHour, double price) {
+        //Constrators
+        public TransactionLine(Guid transactionID, Guid serviceTaskID, Guid enginnerID,double hours, double pricePerHour) {
             ID = Guid.NewGuid();
             TransactionID = transactionID;
             ServiceTaskID = serviceTaskID;
             EnginnerID = enginnerID;
             PricePerHour = pricePerHour;
-            Price = price;
+            Price = pricePerHour * hours;
         }
+        // Methods
     }
 }
