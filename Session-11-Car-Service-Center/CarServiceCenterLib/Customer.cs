87fca6022c96a18c9973501ede253313c27b5050
Sotiris Chrysanthou <sdi1700224@di.uoa.gr>
2023-01-23 17:44:21 +0200

    Customer constractor

--- Session-11-Car-Service-Center/CarServiceCenterLib/Customer.cs
+++ Session-11-Car-Service-Center/CarServiceCenterLib/Customer.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace CarServiceCenterLib {
     public class Customer : Person{
+        // Properties
+        public String Phone { get; set; }
+        public String TIN { get; set; }
 
+        // Constructors
+        public Customer(String name, String surname, String phone, String tin) : base(name, surname) {
+            Phone = phone;
+            TIN = tin;
+        }
+
+        // Methods
     }
 }
