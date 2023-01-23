4c57f04ba762cabd4df467d95cf979912672c7b6
Sotiris Chrysanthou <sdi1700224@di.uoa.gr>
2023-01-23 16:52:49 +0200

    class Persson implemented

---
+++ Session-11-Car-Service-Center/CarServiceCenterLib/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace CarServiceCenterLib {
+    public class Person {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Person(String name, String surname) {
+            ID = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
