f91dab0f206e78642055b3c5d1430dee41822551
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-23 17:18:33 +0200

    Engineer Added

---
+++ Session-11-Car-Service-Center/CarServiceCenterLib/Engineer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenterLib
+{
+    public class Engineer  : Person
+    {
+        //Properties
+        public Guid ManagerID { get; set; }
+        public double SalaryPerMonth { get; set; }
+
+    }
+}
