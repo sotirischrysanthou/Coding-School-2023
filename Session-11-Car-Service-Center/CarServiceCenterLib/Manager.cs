d8e3664c1975a39d0aba17c84155a9a5a881a128
Dimitris Manolas <dimemanolas@gmail.com>
2023-01-23 18:00:21 +0200

    ready Manager

--- Session-11-Car-Service-Center/CarServiceCenterLib/Manager.cs
+++ Session-11-Car-Service-Center/CarServiceCenterLib/Manager.cs
@@ -7,9 +7,20 @@ using System.Threading.Tasks;
 namespace CarServiceCenterLib {
     public class Manager : Person {
 
-        public double SallaryPerMonth { get; set; }
+        //properties
+        public double SalaryPerMonth { get; set; }
+        public List<Engineer> Engineers { get; set; }
 
-        // public List<Engineer> Engineers { get; set; }
+        public Manager(string name, string surname, double salaryPerMonth) : base(name, surname) {
+            Engineers = new List<Engineer>();
+            SalaryPerMonth = salaryPerMonth;
+        }
+
+        //methods
+
+        public void AddEngineer( Engineer engineer ) {
+            Engineers.Add(engineer);
+        } 
     }
 
 
