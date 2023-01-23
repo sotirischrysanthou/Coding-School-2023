f8c0a27812c0e34facaab275d11be515747aaa14
Sotiris Chrysanthou <sdi1700224@di.uoa.gr>
2023-01-23 13:50:52 +0200

    CarService First commit

---
+++ Session-11-Car-Service-Center/Session-11-Car-Service-Center/Form1.cs
@@ -0,0 +1,7 @@
+namespace Session_11_Car_Service_Center {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Session-11-Car-Service-Center/Session-11-Car-Service-Center/Program.cs
@@ -0,0 +1,14 @@
+namespace Session_11_Car_Service_Center {
+    internal static class Program {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
