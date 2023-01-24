c6b71ab8a302655966d1926358e0b77d9b206722
Dimitris Manolas <dimemanolas@gmail.com>
2023-01-24 18:34:54 +0200

    ServiceTasksForm

--- Session-11-Car-Service-Center/Session-11-Car-Service-Center/ServiceTasksForm.cs
+++ Session-11-Car-Service-Center/Session-11-Car-Service-Center/ServiceTasksForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarServiceCenterLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,8 +11,18 @@ using System.Windows.Forms;
 
 namespace Session_11_Car_Service_Center {
     public partial class ServiceTasksForm : Form {
-        public ServiceTasksForm() {
+        private CarServiceCenter _carServiceCenter;
+        public ServiceTasksForm(CarServiceCenter carServiceCenter) {
             InitializeComponent();
+            _carServiceCenter = carServiceCenter;
+        }
+        private void ServiceTasksForm_Load(object sender, EventArgs e) {
+            SetControlProperties();
+        }
+
+        private void SetControlProperties() {
+            bsServiceTasks.DataSource = _carServiceCenter.ServiceTasks;
+            grdServiceTasks.DataSource = bsServiceTasks;
         }
     }
 }
