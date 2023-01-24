ce08d7e15722d2110edcf772490e826e4c698f9c
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-24 13:19:10 +0200

    AddTask Method added

--- Session-11-Car-Service-Center/CarServiceCenterLib/WorkDay.cs
+++ Session-11-Car-Service-Center/CarServiceCenterLib/WorkDay.cs
@@ -30,8 +30,30 @@ namespace CarServiceCenterLib {
         public bool AddTask(ServiceTask task, out String message) {
             //task cannot be over 8 hours
             //(WorkLoad + task.Hours) cannot be > than MaxWorkLoad
+
+            //Initialize message variable
             message = "";
+
+            //Check if task is over 8 hours
+            if (task.Hours > 8) {
+                message = "Task cannot be over 8 hours.";
+                return false;
+            }
+
+            //Check if adding the task will exceed the MaxWorkLoad
+            if (WorkLoad() + task.Hours > MaxWorkLoad()) {
+                message = "Adding this task will exceed the maximum workload.";
+                return false;
+            }
+
+            //If task is valid, add it to the list and update the WorkLoad
+            ServiceTasks.Add(task);
+
+
+            //Return success message
+            message = "Task added successfully";
             return true;
+
         }
     }
 }
