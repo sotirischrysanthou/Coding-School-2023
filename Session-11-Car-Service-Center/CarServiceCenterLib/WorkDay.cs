79730151eca3a7e1db798cc7a65216e344dcfa3e
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-24 12:44:45 +0200

    WorkDay Added

---
+++ Session-11-Car-Service-Center/CarServiceCenterLib/WorkDay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenterLib
+{
+    public class WorkDay
+    {
+        public DateTime Date { get; set; }
+
+        public List<ServiceTask> ServiceTasks { get; set; }
+
+        public int NumOfEngineers { get; set; }
+
+        public double WorkLoad()
+        {
+            double sumWorkHours = 0.0;
+            foreach (ServiceTask serviceTask in ServiceTasks)
+            {
+                sumWorkHours = sumWorkHours + serviceTask.Hours;
+            }
+            return sumWorkHours;
+        }
+        public double MaxWorkLoad()
+        {
+            return NumOfEngineers * 8;
+        }
+
+        public int UpdateNumOfEngineers(int Engineers)
+        {
+            NumOfEngineers = Engineers;
+            return NumOfEngineers;
+        }
+        public bool AddTask(ServiceTask task)
+        {
+            return true;
+        }
+    }
+}
+
