101a926c4fa311944ff3877eca085c495ac105ad
Sotiris Chrysanthou <sdi1700224@di.uoa.gr>
2023-01-24 13:32:11 +0200

    WorkDay.Add() correction

--- Session-11-Car-Service-Center/CarServiceCenterLib/WorkDay.cs
+++ Session-11-Car-Service-Center/CarServiceCenterLib/WorkDay.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,16 +8,17 @@ using System.Threading.Tasks;
 namespace CarServiceCenterLib {
     public class WorkDay {
         public DateTime Date { get; set; }
-
         public List<ServiceTask> ServiceTasks { get; set; }
-
         public int NumOfEngineers { get; set; }
+        private double _workload { get; set; }
+
 
         public double WorkLoad() {
             double sumWorkHours = 0.0;
             foreach (ServiceTask serviceTask in ServiceTasks) {
                 sumWorkHours = sumWorkHours + serviceTask.Hours;
             }
+            _workload = sumWorkHours;
             return sumWorkHours;
         }
         public double MaxWorkLoad() {
@@ -31,29 +33,25 @@ namespace CarServiceCenterLib {
             //task cannot be over 8 hours
             //(WorkLoad + task.Hours) cannot be > than MaxWorkLoad
 
-            //Initialize message variable
-            message = "";
-
-            //Check if task is over 8 hours
-            if (task.Hours > 8) {
+            bool ret = false;
+            //Check if task is less than 8 hours
+            if (task.Hours < 8) {
+                //Check if adding the task will not exceed the MaxWorkLoad
+                if (WorkLoad() + task.Hours < MaxWorkLoad()) {
+                    //If task is valid, add it to the list and update the WorkLoad
+                    ServiceTasks.Add(task);
+                    message = "Task added successfully";
+                    _workload += task.Hours;
+                    ret = true;
+                } else {
+                    message = "Adding this task will exceed the maximum workload.";
+                    ret = false;
+                }
+            } else {
                 message = "Task cannot be over 8 hours.";
-                return false;
+                ret = false;
             }
-
-            //Check if adding the task will exceed the MaxWorkLoad
-            if (WorkLoad() + task.Hours > MaxWorkLoad()) {
-                message = "Adding this task will exceed the maximum workload.";
-                return false;
-            }
-
-            //If task is valid, add it to the list and update the WorkLoad
-            ServiceTasks.Add(task);
-
-
-            //Return success message
-            message = "Task added successfully";
-            return true;
-
+            return ret;
         }
     }
 }
