e612b7533c1e7a0a21838f0dfa3a383aa3e78338
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-26 00:17:50 +0200

    MainMenu

--- Session-11-Car-Service-Center/Session-11-Car-Service-Center/MainMenuForm.Designer.cs
+++ Session-11-Car-Service-Center/Session-11-Car-Service-Center/MainMenuForm.Designer.cs
@@ -210,11 +210,11 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(250, 25);
+            this.label1.Location = new System.Drawing.Point(230, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 26);
+            this.label1.Size = new System.Drawing.Size(126, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "HOME";
+            this.label1.Text = "WELCOME";
             // 
             // pictureBox1
             // 
