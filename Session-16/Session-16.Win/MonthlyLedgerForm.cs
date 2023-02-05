88529edc8052d9ead07f3d0ec5512f91143534a1
Sotiris Chrysanthou <sotririsx10@gmail.com>
2023-02-05 17:14:30 +0200

    Monthly Ledger

--- Session-16/Session-16.Win/MonthlyLedgerForm.cs
+++ Session-16/Session-16.Win/MonthlyLedgerForm.cs
@@ -1,4 +1,5 @@
 ﻿using CarServiceCenterLib.Models;
+using CarServiceCenterLib.Orm.Repositories;
 using DevExpress.Utils.Extensions;
 using DevExpress.XtraBars;
 using DevExpress.XtraCharts.GLGraphics;
@@ -41,14 +42,16 @@ namespace Session_16.Win {
         }
 
         private void btnCalculate_Click(object sender, EventArgs e) {
+            TransactionRepo transactionRepo = new TransactionRepo();
+            List<Transaction> transactions = transactionRepo.GetAll().ToList();
             List<MonthlyLedger> list = new List<MonthlyLedger>();
             MonthlyLedger monthlyLedger;
             int year = DateTime.Now.Year;
             for (int month = 1; month <= 12; month++) {
                 monthlyLedger = new MonthlyLedger(year, month);
-                monthlyLedger.Expenses += _carServiceCenter.SalaryEngineersFrom(year, month);
-                monthlyLedger.Expenses += _carServiceCenter.SalaryManagersFrom(year, month);
-                foreach (Transaction transaction in _carServiceCenter.Transactions) {
+                monthlyLedger.Expenses += SalaryEngineersFrom(year, month);
+                monthlyLedger.Expenses += SalaryManagersFrom(year, month);
+                foreach (Transaction transaction in transactions) {
                     if (transaction.Date.Year == year && transaction.Date.Month == month) {
                         monthlyLedger.Incomes += transaction.TotalPrice;
                     }
@@ -74,5 +77,28 @@ namespace Session_16.Win {
             btnCalculate.FlatAppearance.BorderColor = Color.Black;
             btnCalculate.FlatAppearance.BorderSize = 2;
         }
+        private double SalaryEngineersFrom(int Year, int Month) {
+            double TotalSalary = 0;
+            EngineerRepo engineerRepo = new EngineerRepo();
+            List<Engineer> engineers = engineerRepo.GetAll().ToList();
+            foreach (Engineer engineer in engineers) {
+                if (((DateTime)engineer.StartDate).Year <= Year && ((DateTime)engineer.StartDate).Month <= Month) {
+                    TotalSalary += engineer.SalaryPerMonth;
+                }
+            }
+            return TotalSalary;
+        }
+        private double SalaryManagersFrom(int Year, int Month) {
+            double TotalSalary = 0;
+            ManagerRepo managerRepo = new ManagerRepo();
+            List<Manager> managers = managerRepo.GetAll().ToList();
+            foreach (Manager manager in managers) {
+                if (((DateTime)manager.StartDate).Year <= Year && ((DateTime)manager.StartDate).Month <= Month) {
+                    TotalSalary += manager.SalaryPerMonth;
+                }
+            }
+            return TotalSalary;
+        }
     }
+
 }
