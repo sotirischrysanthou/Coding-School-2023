55475923c828a1486c38f243078904f42b770509
gregavg <gregavg@gmail.com>
2023-02-14 21:09:59 +0200

    Session-27 product completed - without relations

---
+++ Session-27/CoffeShop.Web.Blazor/Server/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using CoffeeShop.EF.Repositories;
+using CoffeeShop.Model;
+using CoffeShop.Web.Blazor.Shared.Product;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoffeShop.Web.Blazor.Server.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase {
+        // Properties
+        private readonly IEntityRepo<Product> _productRepo;
+
+        // Constructors
+        public ProductController(IEntityRepo<Product> productRepo) {
+            _productRepo = productRepo;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public async Task<IEnumerable<ProductListDto>> Get() {
+            var result = _productRepo.GetAll();
+            var selectProductList = result.Select(product => new ProductListDto {
+                Id = product.Id,
+                Code = product.Code,
+                Description = product.Description,
+                Cost = product.Cost,
+                Price = product.Price,
+
+
+            });
+            return selectProductList;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet("{id}")]
+        public async Task<ProductEditDto> GetById(int id) {
+            var result = _productRepo.GetById(id);
+            return new ProductEditDto {
+                Id = id,
+                Code = result.Code,
+                Description = result.Description,
+                Cost = result.Cost,
+                Price = result.Price,
+            };
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public async Task Post(ProductEditDto product) {
+            var newProduct = new Product(product.Code, product.Description, product.Price, product.Cost);
+            _productRepo.Add(newProduct);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut]
+        public async Task Put(ProductEditDto product) {
+            var dbProduct = _productRepo.GetById(product.Id);
+            dbProduct.Code = product.Code;
+            dbProduct.Description = product.Description;
+            dbProduct.Cost = product.Cost;
+            dbProduct.Price = product.Price;
+            _productRepo.Update(product.Id, dbProduct);
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) {
+            _productRepo.Delete(id);
+        }
+    }
+}
---
+++ Session-27/CoffeShop.Web.Blazor/Shared/Product/ProductEditDto.cs
@@ -0,0 +1,23 @@
+﻿using CoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShop.Web.Blazor.Shared.Product {
+    public class ProductEditDto {
+
+        public int Id { get; set; }
+        public String Code { get; set; } = null!;
+        public String Description { get; set; } = null!;
+        public decimal Price { get; set; } 
+        public decimal Cost { get; set; }
+
+        //RELATIONS??????
+        //public int ProductCategoryId { get; set; }
+        //public ProductCategory ProductCategory { get; set; } = null!;
+        //public List<TransactionLine> TransactionLines { get; set; }
+
+    }
+}
