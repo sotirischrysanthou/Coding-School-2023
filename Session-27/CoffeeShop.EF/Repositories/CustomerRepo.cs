85f565b673266830b00dec3b9995279a83c887b9
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-02-14 15:52:51 +0200

    Repos: Product, Product Category, Customer

---
+++ Session-27/CoffeeShop.EF/Repositories/CustomerRepo.cs
@@ -0,0 +1,50 @@
+﻿using CoffeeShop.EF.Context;
+using CoffeeShop.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repositories {
+    public class CustomerRepo : IEntityRepo<Customer> {
+        public void Add(Customer entity) {
+            using var context = new CoffeeShopDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id) {
+            using var context = new CoffeeShopDbContext();
+            var dbCustomer = context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
+            if (dbCustomer is null)
+                return;
+            context.Remove(dbCustomer);
+            context.SaveChanges();
+        }
+
+        public IList<Customer> GetAll() {
+            using var context = new CoffeeShopDbContext();
+            return context.Customers
+                .Include(customer => customer.Transactions).ToList();
+        }
+
+        public Customer? GetById(int id) {
+            using var context = new CoffeeShopDbContext();
+            return context.Customers.Where(customer => customer.Id == id)
+                .Include(customer => customer.Transactions).SingleOrDefault();
+        }
+
+        public void Update(int id, Customer entity) {
+            using var context = new CoffeeShopDbContext();
+            var dbCustomer = context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
+            if (dbCustomer is null)
+                return;
+            dbCustomer.Code = entity.Code;
+            dbCustomer.Description = entity.Description;
+            dbCustomer.Transactions = entity.Transactions;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Session-27/CoffeeShop.EF/Repositories/Product.cs
@@ -0,0 +1,53 @@
+﻿using CoffeeShop.EF.Context;
+using CoffeeShop.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repositories {
+    public class ProductRepo : IEntityRepo<Product> {
+        public void Add(Product entity) {
+            using var context = new CoffeeShopDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id) {
+            using var context = new CoffeeShopDbContext();
+            var dbProduct = context.Products.Where(product => product.Id == id).SingleOrDefault();
+            if (dbProduct is null)
+                return;
+            context.Remove(dbProduct);
+            context.SaveChanges();
+        }
+
+        public IList<Product> GetAll() {
+            using var context = new CoffeeShopDbContext();
+            return context.Products
+                .Include(product => product.TransactionLines).ToList();
+        }
+
+        public Product? GetById(int id) {
+            using var context = new CoffeeShopDbContext();
+            return context.Products.Where(customer => customer.Id == id)
+                .Include(product => product.TransactionLines).SingleOrDefault();
+        }
+
+        public void Update(int id, Product entity) {
+            using var context = new CoffeeShopDbContext();
+            var dbProduct = context.Products.Where(product => product.Id == id).SingleOrDefault();
+            if (dbProduct is null)
+                return;
+            dbProduct.Code = entity.Code;
+            dbProduct.Description = entity.Description;
+            dbProduct.Price = entity.Price;
+            dbProduct.Cost = entity.Cost;
+            dbProduct.ProductCategoryId = entity.ProductCategoryId;
+            dbProduct.ProductCategory = entity.ProductCategory;
+            context.SaveChanges();
+        }
+    }
+}
