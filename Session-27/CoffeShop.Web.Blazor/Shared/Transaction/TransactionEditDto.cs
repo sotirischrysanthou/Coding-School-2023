f5663331bd05faa7f47011e27930ecd2cb6a0987
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-02-15 00:35:16 +0200

    EEEERRRROOOOOORR

---
+++ Session-27/CoffeShop.Web.Blazor/Shared/Transaction/TransactionEditDto.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShop.Model.Enums;
+using CoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShop.Web.Blazor.Shared.Transaction {
+    public class TransactionEditDto {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalPrice { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+
+        // Relations
+        public int CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        //public List<TransactionLine> TransactionLines { get; set; } = null!;
+    }
+}
---
+++ Session-27/CoffeShop.Web.Blazor/Shared/Transaction/TransactionListDto.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeShop.Model.Enums;
+using CoffeeShop.Model;
+
+
+namespace CoffeShop.Web.Blazor.Shared.Transaction {
+    public class TransactionListDto {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalPrice { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+
+        // Relations
+        public CoffeeShop.Model.Customer Customer { get; set; }
+        public CoffeeShop.Model.Employee Employee { get; set; }
+
+        //public List<TransactionLine> TransactionLines { get; set; } = null!;
+    }
+}
