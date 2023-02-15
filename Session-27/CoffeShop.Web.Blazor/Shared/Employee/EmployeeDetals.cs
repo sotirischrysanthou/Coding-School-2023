1968484f87a4bc0f5413bcbc9f89b206903cf029
petran15 <kainourgiogiapame15@gmail.com>
2023-02-15 18:04:45 +0200

    commit Details Customer+Employee

---
+++ Session-27/CoffeShop.Web.Blazor/Shared/Employee/EmployeeDetals.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeShop.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShop.Web.Blazor.Shared.Employee
+{
+    internal class EmployeeDetals
+    {
+        public int Id { get; set; }
+        public String Name { get; set; } = null!;
+        public String Surname { get; set; } = null!;
+        public int SalaryPerMonth { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+    }
+}
