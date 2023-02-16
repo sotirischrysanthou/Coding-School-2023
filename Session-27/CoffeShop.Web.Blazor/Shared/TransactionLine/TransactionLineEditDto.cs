cb6e9202ada4c5ddec34ff78a51dcebbb923b021
gregavg <gregavg@gmail.com>
2023-02-16 20:30:06 +0200

    updates

--- Session-27/CoffeShop.Web.Blazor/Shared/TransactionLine/TransactionLineEditDto.cs
+++ Session-27/CoffeShop.Web.Blazor/Shared/TransactionLine/TransactionLineEditDto.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CoffeShop.Web.Blazor.Shared {
     public class TransactionLineEditDto {
+
+
         public int Id { get; set; }
+
+        [Required]
         public int Quantity { get; set; }
+        [Required]
+        [Range(1, 99.99, ErrorMessage = "You must choose a category!")]
         public decimal Discount { get; set; }
+        [Required]
+        [Range(0, 99999999.99, ErrorMessage = "You must choose a category!")]
         public decimal Price { get; set; }
+        [Required]
+        [Range(0, 99999999.99, ErrorMessage = "You must choose a category!")]
         public decimal TotalPrice { get; set; }
 
         // Relations
