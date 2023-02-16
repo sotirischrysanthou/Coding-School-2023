96c118b7ee4241905574fbacd0891e79d33bdf4a
gregavg <gregavg@gmail.com>
2023-02-16 15:43:13 +0200

    Updates validation

--- Session-27/CoffeShop.Web.Blazor/Shared/Employee/EmployeeEditDto.cs
+++ Session-27/CoffeShop.Web.Blazor/Shared/Employee/EmployeeEditDto.cs
@@ -1,6 +1,7 @@
 ﻿using CoffeeShop.Model.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,19 @@ namespace CoffeShop.Web.Blazor.Shared {
     public class EmployeeEditDto
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Type less than 50 characters")]
         public String Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Type less than 50 characters")]
         public String Surname { get; set; } = null!;
-        public int SalaryPerMonth { get; set; } 
+
+        [Required]
+        [Range(0, 9999, ErrorMessage = "The range is from 0 to 9999!")]
+        public int SalaryPerMonth { get; set; }
+
+        [Required]
         public EmployeeType EmployeeType { get; set; } 
     }
 }
