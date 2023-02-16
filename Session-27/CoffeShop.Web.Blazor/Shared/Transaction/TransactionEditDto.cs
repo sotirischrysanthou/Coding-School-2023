29b151ebd0cb773821e64f5a03da090fa21d73b7
gregavg <gregavg@gmail.com>
2023-02-16 17:38:00 +0200

    Validations except Transaction

--- Session-27/CoffeShop.Web.Blazor/Shared/Transaction/TransactionEditDto.cs
+++ Session-27/CoffeShop.Web.Blazor/Shared/Transaction/TransactionEditDto.cs
@@ -6,12 +6,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CoffeShop.Web.Blazor.Shared;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoffeShop.Web.Blazor.Shared {
     public class TransactionEditDto {
+
         public int Id { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
+        [Range(0, 99999999.99, ErrorMessage = "The range is from 0 to 99999999.99!")]
         public decimal TotalPrice { get; set; }
+
+        [Required]
+        [Range(1, 2, ErrorMessage = "You must choose a payment method!")]
         public PaymentMethod PaymentMethod { get; set; }
 
         // Relations
